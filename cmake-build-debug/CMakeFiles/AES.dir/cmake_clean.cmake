4c2271e0747f374b3483f5d15fbfbc0a83e625e3
summerainnn <709492145@qq.com>
2020-12-03 20:45:50 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/AES.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AES"
+  "AES.pdb"
+  "CMakeFiles/AES.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
